655724bf21b9cfdad4c70d9726c62b636c6053e4
eldinor <eldinor@gmail.com>
2024-11-18 00:48:28 +0300

    canvas-drop-ok

--- src/CanvasViewer.tsx
+++ src/CanvasViewer.tsx
@@ -3,8 +3,10 @@ import { useEffect, useRef } from "react";
 import { Viewer, createViewerForCanvas } from "@babylonjs/viewer";
 
 function CanvasViewer(props: { source: string | ArrayBufferView | File; orbit: boolean }) {
+  
   const canvasRef = useRef(null);
   const viewerRef = useRef<Viewer | null>(null);
+
   if (viewerRef.current) {
     viewerRef.current.cameraAutoOrbit = { enabled: props.orbit };
   }
@@ -12,7 +14,10 @@ function CanvasViewer(props: { source: string | ArrayBufferView | File; orbit: b
   useEffect(() => {
     if (!canvasRef.current) return;
 
-    const viewerPromise = createViewerForCanvas(canvasRef.current, {engine:"WebGPU"});
+    const viewerPromise = createViewerForCanvas(canvasRef.current, { engine: "WebGPU", onInitialized:(details)=>{
+console.log(details)
+details.scene.clearColor.a = 0
+    } });
 
     viewerPromise.then((viewer) => {
       viewer.loadModel(props.source);
@@ -27,7 +32,26 @@ function CanvasViewer(props: { source: string | ArrayBufferView | File; orbit: b
     return () => {};
   }, [canvasRef]);
 
-  return <canvas ref={canvasRef} style={{ width: "100%", height: "100%" }} />;
+  return (
+    <div
+      onDragOver={(e) => {
+        e.preventDefault();
+        console.log("onDragOver");
+      }}
+      onDrop={(e) => {
+        console.log("onDrop");
+        const file = e.dataTransfer.files[0];
+        if (file) {
+          e.preventDefault();
+          if (viewerRef.current) {
+            viewerRef.current?.loadModel(file);
+          }
+        }
+      }}
+    >
+      <canvas ref={canvasRef} style={{ width: "100%", height: "100%",backgroundColor:"MediumPurple",outline:"none" }} />
+    </div>
+  );
 }
 
 export default CanvasViewer;
