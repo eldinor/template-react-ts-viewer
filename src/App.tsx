38dea57fe324d0f9b34cb8558ffdd9320fef2f6a
eldinor <eldinor@gmail.com>
2024-11-17 03:51:55 +0300

    Babylon-viewer tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import ViewerComponent from './ViewerComponent';
+import "./App.css";
+import { HTML3DElement } from "@babylonjs/viewer";
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "babylon-viewer": HTML3DElementAttributes;
+    }
+  }
+}
+interface HTML3DElementAttributes extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
+  source?: string;
+  env?: string;
+}
 
 export default function App() {
-  const [orbitSpeed, setOrbitSpeed] = useState(false);
+  new HTML3DElement();
   return (
-<>
-        <ViewerComponent
-               orbit={orbitSpeed}
-               source={'https://playground.babylonjs.com/scenes/BoomBox.glb'}
-        />
-        <p></p>
-      <button
-        type="button"
-        onClick={() => {
-          setOrbitSpeed((arg) => !arg);
-        }}
-      >
-        {orbitSpeed ? 'Stop' : 'Rotate'}
-      </button>
-</>
+    <>
+      <babylon-viewer source="https://raw.githubusercontent.com/eldinor/ForBJS/master/office_chair_modern-opt.glb"></babylon-viewer>
+    </>
   );
 }
